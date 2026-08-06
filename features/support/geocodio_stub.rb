b7d9e46f10e84024193d28b63de4ae2ca97b5a82
Erick Hernandez Hidalgo <erickh112305@berkeley.edu>
2026-08-06 04:04:44 +0000

    I1 and I2 done

--- features/support/geocodio_stub.rb
+++ features/support/geocodio_stub.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 require 'webmock/cucumber'
+
 WebMock.disable_net_connect!(allow_localhost: true)
+
+ENV['GEOCODIO_API_KEY'] ||= 'test-key-not-used'
+
+GEOCODIO_FIXTURE = Rails.root.join('spec/fixtures/geocodio_response.json')
+
+Before do
+  # The geocodio-gem client POSTs. A :get stub will not match, and the request
+  # will still try to reach the network.
+  stub_request(:post, /api\.geocod\.io/).to_return(
+    status: 200,
+    body: GEOCODIO_FIXTURE.read,
+    headers: { 'Content-Type' => 'application/json' }
+  )
+end
