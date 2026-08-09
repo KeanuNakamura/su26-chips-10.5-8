eb65bf9f797d2142efa17b405b3616fd4c93679b
Keanu Nakamura <keanunakamura@berkeley.edu>
2026-08-09 03:12:22 +0000

    tests for storing representative information from geocodio

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -27,7 +27,19 @@ RSpec.describe Representative do
   describe '.find_rep' do
     let(:official) do
       {
-        'name' => 'Jane Doe'
+        'name' => 'Jane Doe',
+        'type' => 'representative', 
+        'bio' => {'party' => 'Democrat'},
+        'contact' => {
+          'address' =>'123 Test, California 2932', 
+          'phone' =>'123-456-7891',
+          'url' => 'https://lol.blah.gov'
+        },
+        'social' => {'twitter' => 'meow'}, 
+        'references' => {
+          'govtrack_id' => '412345',
+          'bioguide_id' => 'B00039'
+        }
       }
     end
 
@@ -46,6 +58,17 @@ RSpec.describe Representative do
       expect do
         described_class.find_rep(official, **args)
       end.not_to change(described_class, :count)
+    end
+    it 'stores representative information from Geocodio' do
+      rep = described_class.find_by(ocdid: '412345')
+      expect(rep.name).to eq('Jane Doe')
+      expect(rep.title).to eq('representative')
+      expect(rep.party).to eq('Democrat')
+      expect(rep.address).to eq('123 Test, California 2932')
+      expect(rep.phone).to eq('123-456-7891')
+      expect(rep.website).to eq('https://lol.blah.gov')
+      expect(rep.twitter).to eq('meow')
+      expect(rep.photo_url).to include('B00039')
     end
   end
 end
